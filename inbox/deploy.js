a33499b734ea2d8f2d9348145818cf8ba33b8625
Gauthier Riou <gauthier.riou@airbus.com>
2019-09-25 15:08:04 +0200

    feat: Deploy contract to rinkeby

---
+++ inbox/deploy.js
@@ -0,0 +1,27 @@
+const HDWalletProvider = require('@truffle/hdwallet-provider')
+const Web3 = require('web3')
+const fs = require('fs')
+const path = require('path')
+const { interface, bytecode } = require('./compile')
+
+const mnemonic = fs.readFileSync('../.mnemonic').toString().trim()
+const infuraKey = fs.readFileSync('../.infuraKey').toString().trim()
+
+const provider = new HDWalletProvider(
+  mnemonic,
+  `https://rinkeby.infura.io/${infuraKey}`
+)
+const web3 = new Web3(provider)
+
+// define function so that we can use await/async syntax (optional)
+const deploy = async () => {
+  const accounts = await web3.eth.getAccounts()
+  console.log(accounts)
+  // const inbox = await new web3.eth.Contract(JSON.parse(interface))
+  // .deploy({ data: bytecode, arguments: ['Deploying to rinkeby!!'] })
+  // .send({ from: accounts[0], gas: '1000000'})
+  // console.log('Contract deployed to', inbox.options.address)
+}
+deploy()
+
+// deploy({ data: '0x' +...}) ??
