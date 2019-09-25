92373e87bf087a614f061cb2db38062402f28c16
Gauthier Riou <gauthier.riou@airbus.com>
2019-09-25 12:51:12 +0200

    test: Deploy contract and initialize with default message

--- inbox/test/Inbox.test.js
+++ inbox/test/Inbox.test.js
@@ -1,30 +1,29 @@
 const assert = require('assert')
 const ganache = require('ganache-cli')
 const Web3 = require('web3')
+const { interface, bytecode } = require('../compile')
 
 const web3 = new Web3(ganache.provider())
 
-class Car {
-  park () {
-    return 'stopped'
-  }
+let accounts
+let inbox
+beforeEach(async () => {
+  // get accounts
+  accounts = await web3.eth.getAccounts()
 
-  drive () {
-    return 'vrrom'
-  }
-}
-
-let car
-beforeEach(() => {
-  car = new Car()
+  // use one account for deployment
+  inbox = await new web3.eth.Contract(JSON.parse(interface)) // argument= ABI as JS (needs to be parsed). Tells web3 about what methods an inbox contract has. But tells nothing about what data is in the contract or when it was deployed...
+    .deploy({ data: bytecode, arguments: ['Hi There'] }) // tells web3 we want to deploy a new contract, arguments= array of arguments for the constructor of the contract
+    .send({ from: accounts[0], gas: '1000000' }) // instructs web3 to send out tx to actually create instance of contract
 })
 
-describe('Car', () => {
-  it('Can park', () => {
-    assert.equal(car.park(), 'stopped')
+describe('Inbox', () => {
+  it('can deploy contract', () => {
+    assert.ok(inbox.options.address)
   })
 
-  it('Can drive', () => {
-    assert.equal(car.drive(), 'vrrom')
+  it('initializes with default message', async () => {
+    const message = await inbox.methods.message().call()
+    assert.equal(message, 'Hi There')
   })
 })
