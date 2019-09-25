3112adac4a7fea74b2f38f6b3f963ed680aa2d97
6ry0u <6ry0u@tutanota.com>
2019-09-25 20:56:06 +0200

    fix: Deploy contract to rinkeby

--- inbox/deploy.js
+++ inbox/deploy.js
@@ -4,8 +4,14 @@ const fs = require('fs')
 const path = require('path')
 const { interface, bytecode } = require('./compile')
 
-const mnemonic = fs.readFileSync('../.mnemonic').toString().trim()
-const infuraKey = fs.readFileSync('../.infuraKey').toString().trim()
+const mnemonic = fs
+  .readFileSync('../.mnemonic')
+  .toString()
+  .trim()
+const infuraKey = fs
+  .readFileSync('../.infuraKey')
+  .toString()
+  .trim()
 
 const provider = new HDWalletProvider(
   mnemonic,
@@ -16,12 +22,10 @@ const web3 = new Web3(provider)
 // define function so that we can use await/async syntax (optional)
 const deploy = async () => {
   const accounts = await web3.eth.getAccounts()
-  console.log(accounts)
-  // const inbox = await new web3.eth.Contract(JSON.parse(interface))
-  // .deploy({ data: bytecode, arguments: ['Deploying to rinkeby!!'] })
-  // .send({ from: accounts[0], gas: '1000000'})
-  // console.log('Contract deployed to', inbox.options.address)
+  console.log('Trying to deploy from account', accounts[0])
+  const inbox = await new web3.eth.Contract(JSON.parse(interface))
+    .deploy({ data: '0x' + bytecode, arguments: ['Deploying to rinkeby!!'] })
+    .send({ from: accounts[0], gas: '1000000' })
+  console.log('Contract deployed to', inbox.options.address)
 }
 deploy()
-
-// deploy({ data: '0x' +...}) ??
