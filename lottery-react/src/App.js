38bb38992dae5abdd1bf0b0406bff702c8d5bc3f
6ry0u <6ry0u@tutanota.com>
2019-09-28 14:47:50 +0200

    feat: Can enter the lottery (front end)

--- lottery-react/src/App.js
+++ lottery-react/src/App.js
@@ -5,13 +5,29 @@ import './App.css'
 import lottery from './lottery'
 
 class App extends Component {
-  state = { manager: '', players: [], balance: '' }
+  state = { 
+    manager: '',
+    players: [],
+    balance: '',
+    value: '' 
+  }
 
   async componentDidMount () {
     const manager = await lottery.methods.manager().call()
     const players = await lottery.methods.getPlayers().call()
     const balance = await web3.eth.getBalance(lottery.options.address)
     this.setState({ manager, players, balance })
+  }
+
+  onSubmit = async event => {
+    event.preventDefault()
+    const accounts = await web3.eth.getAccounts()
+    this.setState({ message: 'Waiting on transaction success...'})
+    await lottery.methods.enter().send({
+      from: accounts[0],
+      value: web3.utils.toWei(this.state.value, 'ether')
+    })
+    this.setState({ message: 'You have been successfully entered.' })
   }
 
   render () {
@@ -21,6 +37,21 @@ class App extends Component {
         <p>This contract is managed by {this.state.manager}</p>
         <p>Players entered: {this.state.players.length}</p>
         <p>Pool: {web3.utils.fromWei(this.state.balance, 'ether')} ETH</p>
+        <hr/>
+        <form onSubmit={this.onSubmit}>
+          <h4>Want to try your luck?</h4>
+          <div>
+            <label htmlFor=''>Amount of ether to enter:</label>
+            <input
+              type='text'
+              value={this.state.value} // controlled input
+              onChange={event => this.setState({value: event.target.value})}
+              />
+          </div>
+          <button>Enter</button>
+        </form>
+        <hr/>
+        <h1>{this.state.message}</h1>
       </div>
     )
   }
